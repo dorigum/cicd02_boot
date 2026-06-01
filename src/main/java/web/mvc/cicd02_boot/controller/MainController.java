4c1856cd7c276bd9aaac6ec5a570367c811c6a41
PolarBear <kkamang03@gmail.com>
2026-06-01 10:07:49 +0900

    ddd

---
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -0,0 +1,12 @@
+package web.mvc.cicd02_boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String index(){
+        return "GitHub Actions를 이용한 CI/CD 테스트입니다. - 왜 벌써 6월이죠??";
+    }
+}
