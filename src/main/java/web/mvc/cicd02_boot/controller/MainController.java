2cd27c04f18b6e0a423b2c5a5b88f7e3e67556ee
PolarBear <kkamang03@gmail.com>
2026-06-01 10:16:05 +0900

    gitActions 빌드

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,10 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-<<<<<<< HEAD
         return "GitHub Actions를 이용한 CI/CD 테스트입니다. - 왜 벌써 6월이죠??";
-=======
-        return "GitHub Actions를 이용한 CI/CD 테스트입니다. - failed check";
->>>>>>> e7bf2f2a9d6a768c16da62103233463ed7d0cce0
     }
 }
--- src/test/java/web/mvc/cicd02_boot/Cicd02BootApplicationTests.java
+++ src/test/java/web/mvc/cicd02_boot/Cicd02BootApplicationTests.java
@@ -8,10 +8,6 @@ class Cicd02BootApplicationTests {
 
     @Test
     void contextLoads() {
-<<<<<<< HEAD
 //        throw new RuntimeException("Fail!");
-=======
-        throw new RuntimeException("Fail!");
->>>>>>> e7bf2f2a9d6a768c16da62103233463ed7d0cce0
     }
 }
